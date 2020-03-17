24ef37a7de78a457f6676cc8639cedcbaa63b356
kasrasamareh <kasragol82@gmail.com>
2020-03-17 12:28:35 +0000

    Remove old stuff

--- game/settings.py
+++ game/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '05pvavnoe2n5zv!jg1g)a+75^p20l=b=p6cky@4#^fs@*+*huz'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['gamegami30.pythonanywhere.com']
 
 
 # Application definition
@@ -119,3 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR , 'static')
